862804e9ba1481586defeace5335dddb4131a079
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-22 19:01:19 -0400

    Added plugin wrapper Added enchantment registration

--- src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -1,12 +1,6 @@
 package com.github.ukraine1449.magicplugin;
 
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
 
 public final class MagicPlugin extends JavaPlugin {
     @Override
