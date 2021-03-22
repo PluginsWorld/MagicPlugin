bd8dd60d38693294bc4badedb8a7d2103d10bbe9
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-22 14:12:04 -0400

    Initial commit

---
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -0,0 +1,17 @@
+package com.github.ukraine1449.magicplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Plugin s starting up");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
