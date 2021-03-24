b02704ec59652c4bc74c67b3b8fd2918c20810a5
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-24 18:22:07 -0400

    Registered command and event

--- src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -1,7 +1,9 @@
 package com.github.ukraine1449.magicplugin;
 
 import com.github.ukraine1449.magicplugin.Commands.Hammer;
+import com.github.ukraine1449.magicplugin.Commands.Shield;
 import com.github.ukraine1449.magicplugin.Events.HammerEvent;
+import com.github.ukraine1449.magicplugin.Events.ShieldEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MagicPlugin extends JavaPlugin {
@@ -9,7 +11,14 @@ public final class MagicPlugin extends JavaPlugin {
     public void onEnable() {
         System.out.println("Plugin is starting up");
         getServer().getPluginManager().registerEvents(new HammerEvent(), this);
+        getServer().getPluginManager().registerEvents(new ShieldEvent(), this);
+
+
+
+
+
         getCommand("hammer").setExecutor(new Hammer());
+        getCommand("shield").setExecutor(new Shield());
     }
 
     @Override
