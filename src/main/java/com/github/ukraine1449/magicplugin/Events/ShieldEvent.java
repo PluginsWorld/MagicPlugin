55cb1a870de2d64b883c9f9c9e8c6007164fc958
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-24 18:20:55 -0400

    Added corresponding shield event for shield of darkness

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/ShieldEvent.java
@@ -0,0 +1,36 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class ShieldEvent implements Listener {
+
+    @EventHandler
+    public void onShieldUse(EntityDamageByEntityEvent event){
+            if(event.getDamager() instanceof Player){
+                Player player = (Player) event.getDamager();
+                String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+                if(mainhand.equals(ChatColor.BLACK + "The shield of darkness")){
+                    Bukkit.broadcastMessage("The shield has been activated");
+                }
+            }
+    }
+
+
+    @EventHandler
+    public void onShieldEquip(PlayerItemHeldEvent event){
+        Player player = event.getPlayer();
+        String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+        if(mainhand.equals(ChatColor.BLACK + "The shield of darkness")){
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 2, 1));
+        }
+    }
+
+}
