f4ee3f0c97b0b3a17bfd3e499fa6203726f6029d
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-28 10:19:14 -0400

    Added Event and command for bow

--- src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -1,10 +1,14 @@
 package com.github.ukraine1449.magicplugin;
 
+import com.github.ukraine1449.magicplugin.Commands.Bow;
 import com.github.ukraine1449.magicplugin.Commands.Hammer;
 import com.github.ukraine1449.magicplugin.Commands.Shield;
+import com.github.ukraine1449.magicplugin.Events.BowEvent;
 import com.github.ukraine1449.magicplugin.Events.HammerEvent;
 import com.github.ukraine1449.magicplugin.Events.ShieldEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Timer;
 
 public final class MagicPlugin extends JavaPlugin {
     @Override
@@ -12,15 +16,18 @@ public final class MagicPlugin extends JavaPlugin {
         System.out.println("Plugin is starting up");
         getServer().getPluginManager().registerEvents(new HammerEvent(), this);
         getServer().getPluginManager().registerEvents(new ShieldEvent(), this);
-
+        getServer().getPluginManager().registerEvents(new BowEvent(), this);
 
 
 
 
         getCommand("hammer").setExecutor(new Hammer());
         getCommand("shield").setExecutor(new Shield());
+        getCommand("bow").setExecutor(new Bow());
     }
-
+        public static void Cooldown(){
+        Timer timer;
+        }
     @Override
     public void onDisable() {
 
