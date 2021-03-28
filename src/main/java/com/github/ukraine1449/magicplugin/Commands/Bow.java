cbbc70cfb0f5486b7b44568d2d0838a2734e5621
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-28 09:44:40 -0400

    Added bow class

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Commands/Bow.java
@@ -0,0 +1,32 @@
+package com.github.ukraine1449.magicplugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Bow implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("Magic.Bow")){
+                ItemStack bow = new ItemStack(Material.BOW);
+                ItemMeta bm = bow.getItemMeta();
+                bm.setDisplayName(ChatColor.LIGHT_PURPLE + "Cupid's Bow");
+                bow.setItemMeta(bm);
+                Inventory playerinv = player.getInventory();
+                playerinv.addItem(bow);
+            }
+        }
+
+        return true;
+    }
+}
