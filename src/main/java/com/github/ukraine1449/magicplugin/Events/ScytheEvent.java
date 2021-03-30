b3737fd2b09cb2aaefd08f68a59c5f43ea154735
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-29 20:39:00 -0400

    Created Scythe event

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/ScytheEvent.java
@@ -0,0 +1,41 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class ScytheEvent implements Listener {
+
+    @EventHandler
+    public void ScytheEvent(EntityDamageByEntityEvent event){
+
+        if(event.getDamager() instanceof Player){
+            Player player = (Player) event.getDamager();
+            if(player.getItemInHand().getItemMeta() == null){
+                return;
+            }else{
+                String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+                if(mainhand.equals(ChatColor.BLACK + "The shield of darkness")){
+                    Entity target = event.getEntity();
+                    if(target instanceof Player){
+                        Player tp = (Player) target;
+                        tp.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 5));
+                        tp.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 5));
+                        tp.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 200, 3));
+                    }
+
+                }
+            }
+        }else{
+            return;
+        }
+
+
+    }
+
+}
