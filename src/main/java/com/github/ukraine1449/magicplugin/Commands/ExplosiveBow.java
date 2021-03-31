57b75152f16c1f7b2837e607ee1e91cce092b691
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-31 12:43:24 -0400

    Created explosive bow

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Commands/ExplosiveBow.java
@@ -0,0 +1,33 @@
+package com.github.ukraine1449.magicplugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ExplosiveBow implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("Magic.ExB")){
+
+                ItemStack bow = new ItemStack(Material.BOW);
+                ItemMeta bm = bow.getItemMeta();
+                bm.setDisplayName(ChatColor.RED + "Boom bow");
+                bow.setItemMeta(bm);
+                player.getInventory().addItem(bow);
+
+
+            }
+        }
+
+        return true;
+    }
+}
