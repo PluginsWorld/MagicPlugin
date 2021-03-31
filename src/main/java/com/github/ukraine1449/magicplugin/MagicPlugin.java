3ef3564e27f3c49317115f848660d7690953f013
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-30 20:20:07 -0400

    Registered scythe and shears

--- src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -1,13 +1,8 @@
 package com.github.ukraine1449.magicplugin;
 
-import com.github.ukraine1449.magicplugin.Commands.Bow;
-import com.github.ukraine1449.magicplugin.Commands.Hammer;
-import com.github.ukraine1449.magicplugin.Commands.HelmetOfVision;
-import com.github.ukraine1449.magicplugin.Commands.Shield;
-import com.github.ukraine1449.magicplugin.Events.BowEvent;
-import com.github.ukraine1449.magicplugin.Events.ESPHelmet;
-import com.github.ukraine1449.magicplugin.Events.HammerEvent;
-import com.github.ukraine1449.magicplugin.Events.ShieldEvent;
+import com.github.ukraine1449.magicplugin.Commands.*;
+import com.github.ukraine1449.magicplugin.Events.*;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MagicPlugin extends JavaPlugin {
@@ -18,13 +13,19 @@ public final class MagicPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ShieldEvent(), this);
         getServer().getPluginManager().registerEvents(new BowEvent(), this);
         getServer().getPluginManager().registerEvents(new ESPHelmet(), this);
+        getServer().getPluginManager().registerEvents(new ShearsEvent(), this);
+        getServer().getPluginManager().registerEvents(new ScytheEvent(), this);
 
 
+        getCommand("scythe").setExecutor(new Scythe());
+        getCommand("shears").setExecutor(new HeadCutterShears());
         getCommand("EH").setExecutor(new HelmetOfVision());
         getCommand("hammer").setExecutor(new Hammer());
         getCommand("shield").setExecutor(new Shield());
         getCommand("bow").setExecutor(new Bow());
     }
+
+
     @Override
     public void onDisable() {
 
