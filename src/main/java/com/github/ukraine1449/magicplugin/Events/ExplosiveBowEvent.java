7cab39f6b191bae17f628c395f17321c5f6cf0ea
Ukraine1449 <roma.bystriakov@gmail.com>
2021-03-31 12:57:38 -0400

    Created explosion at hit location

--- src/main/java/com/github/ukraine1449/magicplugin/Events/ExplosiveBowEvent.java
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/ExplosiveBowEvent.java
@@ -1,10 +1,18 @@
 package com.github.ukraine1449.magicplugin.Events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class ExplosiveBowEvent implements Listener {
 
@@ -15,7 +23,24 @@ public class ExplosiveBowEvent implements Listener {
             if(event.getEntity().getShooter() instanceof Player){
                 Player player = (Player) event.getEntity().getShooter();
 
+                if(player.getItemInHand().getItemMeta() == null){
+                    return;
+                }else{
+                    String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+                    if(mainhand.equals(ChatColor.RED + "Boom bow")){
 
+                        World w = player.getWorld();
+                        Location loc = event.getHitBlock().getLocation();
+                        w.createExplosion(loc, 2, false);
+
+
+
+
+
+                    }else{
+                        return;
+                    }
+                }
 
 
 
