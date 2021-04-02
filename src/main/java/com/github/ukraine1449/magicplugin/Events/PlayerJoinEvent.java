83d95671b4453b175b66823e6403dead3431ae3c
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-01 20:35:29 -0400

    Created player join event

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/PlayerJoinEvent.java
@@ -0,0 +1,25 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import com.github.ukraine1449.magicplugin.MagicPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerJoinEvent implements Listener {
+MagicPlugin plugin;
+
+
+    public PlayerJoinEvent(MagicPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        for(int i = 0; i < plugin.vanished.size(); i++){
+            player.hidePlayer(plugin, plugin.vanished.get(i));
+            player.hidePlayer(player);
+        }
+    }
+
+}
