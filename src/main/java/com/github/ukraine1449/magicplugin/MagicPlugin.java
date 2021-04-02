c0202ae1946d4d5da1b800f2416e7fb74f3bc384
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-01 20:34:50 -0400

    Registered commands/events

--- src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
+++ src/main/java/com/github/ukraine1449/magicplugin/MagicPlugin.java
@@ -2,10 +2,14 @@ package com.github.ukraine1449.magicplugin;
 
 import com.github.ukraine1449.magicplugin.Commands.*;
 import com.github.ukraine1449.magicplugin.Events.*;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+
 public final class MagicPlugin extends JavaPlugin {
+    public ArrayList<Player> vanished = new ArrayList<>();
     @Override
     public void onEnable() {
         System.out.println("Plugin is starting up");
@@ -16,11 +20,12 @@ public final class MagicPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ShearsEvent(), this);
         getServer().getPluginManager().registerEvents(new ScytheEvent(), this);
         getServer().getPluginManager().registerEvents(new ExplosiveBowEvent(), this);
+        getServer().getPluginManager().registerEvents(new InvisEvent(this), this);
 
 
 
 
-
+        getCommand("invis").setExecutor(new Invis());
         getCommand("boombow").setExecutor(new ExplosiveBow());
         getCommand("scythe").setExecutor(new Scythe());
         getCommand("shears").setExecutor(new HeadCutterShears());
