ef7d7489caf4d63c9cfb57859aff24a9cbcd5f53
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-06 17:48:54 -0400

    Created zeus bow event

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/ZeusBowEvent.java
@@ -0,0 +1,53 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class ZeusBowEvent implements Listener {
+
+    @EventHandler
+    public void onBowHit(ProjectileHitEvent event){
+
+        if(event.getEntity() instanceof Arrow){
+            if(event.getEntity().getShooter() instanceof Player){
+                Player player = (Player) event.getEntity().getShooter();
+
+                if(player.getItemInHand().getItemMeta() == null){
+                    return;
+                }else{
+                    String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+                    if(mainhand.equals(ChatColor.RED + "Boom bow")){
+
+                        World w = player.getWorld();
+                        if(event.getHitBlock() == null){
+                            return;
+                        }else{
+                            Location loc = event.getHitBlock().getLocation();
+                            w.strikeLightning(loc);
+                            event.getEntity().remove();
+                        }
+
+
+
+                    }else{
+                        return;
+                    }
+                }
+            }else{
+                return;
+            }
+        }else{
+            return;
+        }
+
+
+
+    }
+
+}
