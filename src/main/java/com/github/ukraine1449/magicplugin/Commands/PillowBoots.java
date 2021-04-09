ee7d9e46ab17917dd22234d05153e56044d992ed
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-08 22:41:31 -0400

    Created boots event and command

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Commands/PillowBoots.java
@@ -0,0 +1,40 @@
+package com.github.ukraine1449.magicplugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class PillowBoots implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("Magic.Pillow")){
+                ItemStack boots = new ItemStack(Material.CHAINMAIL_BOOTS);
+                ItemMeta bm = boots.getItemMeta();
+                bm.setDisplayName(ChatColor.BOLD + "Pillow boots");
+                ArrayList<String> lore = new ArrayList();
+                lore.add(ChatColor.WHITE + "Wear to gain better falling capablities");
+                bm.setLore(lore);
+                boots.setItemMeta(bm);
+                player.getInventory().addItem(boots);
+
+
+
+
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/BootsFallEvent.java
@@ -0,0 +1,33 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.List;
+
+public class BootsFallEvent implements Listener {
+
+    @EventHandler
+    public void onBootsUse(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        if(player.getEquipment().getBoots() == null){
+            return;
+        }else{
+            String Boots = player.getEquipment().getBoots().getItemMeta().getDisplayName();
+            if(Boots.equals(ChatColor.BLUE + "The all seeing helmet")){
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 60, 1));
+            }else if (Boots == null){
+                return;
+            }else{
+                return;
+            }
+        }
+    }
+
+}
