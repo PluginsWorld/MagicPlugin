2fb3c59d7903581911b6f735db432f7161c66f53
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-11 16:07:39 -0400

    Created slow stick class

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Commands/SlowStick.java
@@ -0,0 +1,34 @@
+package com.github.ukraine1449.magicplugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class SlowStick implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("Magic.slowstick")){
+                ItemStack stick = new ItemStack(Material.STICK);
+                ItemMeta sm = stick.getItemMeta();
+                sm.setDisplayName(ChatColor.BOLD + "Feet whacker");
+                ArrayList<String> lore = new ArrayList();
+                lore.add(ChatColor.BOLD + "A stick to give opponenets slowness");
+                sm.setLore(lore);
+                player.getInventory().addItem(stick);
+            }
+        }
+
+        return true;
+    }
+}
