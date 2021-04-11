ba660ec6125ce59c9a9bd479ff3cb4522c49451c
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-11 01:21:14 -0400

    Created pickaxe item and event

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Commands/PickAxe.java
@@ -0,0 +1,33 @@
+package com.github.ukraine1449.magicplugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class PickAxe implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(player.hasPermission("Magic.Pickaxe")){
+                ItemStack pick = new ItemStack(Material.GOLDEN_PICKAXE);
+                ItemMeta pm = pick.getItemMeta();
+                pm.setDisplayName(ChatColor.GOLD + "Excavator");
+                ArrayList<String> lore = new ArrayList();
+                lore.add(ChatColor.GOLD + "Use this to mine large ammounts of block");
+                lore.add(ChatColor.AQUA + "It has an *explosive* surprise :)");
+                pm.setLore(lore);
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/PickaxeEvent.java
@@ -0,0 +1,33 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class PickaxeEvent implements Listener {
+
+    public static void onBlockBreak(BlockBreakEvent event){
+            Player player = event.getPlayer();
+            if(player.getItemInHand().getItemMeta() == null){
+                return;
+            }else{
+                String mainhand = player.getItemInHand().getItemMeta().getDisplayName();
+                if(mainhand.equals(ChatColor.GOLD + "Excavator")){
+
+                    Location loc = event.getBlock().getLocation();
+                    World w = player.getWorld();
+                    w.createExplosion(loc, 2, false);
+                }else{
+                    return;
+                }
+            }
+    }
+
+
+}
