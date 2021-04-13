1fb2878ce44ce404b2018e85f1659073774adf1a
Ukraine1449 <roma.bystriakov@gmail.com>
2021-04-13 13:42:31 -0400

    Revert "Deleting/combining events"
    This reverts commit 30d6782b07fad5097f3350fdff9958b76e19d915.

---
+++ src/main/java/com/github/ukraine1449/magicplugin/Events/ShieldArmEvent.java
@@ -0,0 +1,31 @@
+package com.github.ukraine1449.magicplugin.Events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class ShieldArmEvent implements Listener {
+
+    @EventHandler
+    public void EH(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        if(player.getEquipment().getBoots() == null){
+            return;
+        }else{
+            String Helmet = player.getEquipment().getBoots().getItemMeta().getDisplayName();
+            if(Helmet.equals(ChatColor.LIGHT_PURPLE + "Porta-shield")){
+                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1200, 20));
+                player.getEquipment().setBoots(null);
+            }else if (Helmet == null){
+                return;
+            }else{
+                return;
+            }
+        }
+    }
+
+}
